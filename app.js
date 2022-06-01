c904e4a1b6f19778002e451779fcd39566c0f48f
Andrew Wanex <awanex2705@gmail.com>
2022-06-01 14:29:25 -0700

    feature: v1 server app

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors')
 const express = require('express')
+const bodyParser = require('body-parser')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
@@ -7,14 +8,19 @@ const mongoose = require('mongoose')
 const Bluebird = require('bluebird')
 mongoose.Promise = Bluebird
 const passport = require('passport')
+const customErrorHandler = require('./shared/custom-error-handlers/custom-error-handler')
 
 const apiVersion = '0.0.1'
 const apiRoute = `draft_list_${apiVersion}`
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
+const beverageRouter = require('./routes/beverage')
+const deviceRouter = require('./routes/device')
+const draftRouter = require('./routes/draft')
+const imageRouter = require('./routes/image')
+const containerRouter = require('./routes/container')
 
-let db
 const connect = mongoose.connect(
   process.env.MONGO_URL,
   {
@@ -26,11 +32,9 @@ const connect = mongoose.connect(
     useCreateIndex: true
   }
 )
+
 connect.then(
-  () => {
-    console.log(`Draft List ${apiVersion} database connection established`)
-    db = mongoose.connection
-  },
+  () => console.log(`Draft List ${apiVersion} database connection established`),
   error => console.log(error)
 )
 
@@ -40,6 +44,7 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+app.use(bodyParser.json())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -49,11 +54,27 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(passport.initialize())
 
 app.use('/', indexRouter)
-app.use(`/${apiVersion}/users`, usersRouter)
+app.use(`/${apiRoute}/users`, usersRouter)
+app.use(`/${apiRoute}/drafts`, draftRouter)
+app.use(`/${apiRoute}/devices`, deviceRouter)
+app.use(`/${apiRoute}/beverages`, beverageRouter)
+app.use(`/${apiRoute}/images`, imageRouter)
+app.use(`/${apiRoute}/containers`, containerRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404))
+})
+
+// mongo error handler
+app.use((err, req, res, next) => {
+  return next(customErrorHandler(err))
+})
+
+// error logger
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  return next(err)
 })
 
 // error handler
@@ -62,9 +83,15 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
+  // send error response
+  if (req.url.includes(apiRoute)) {
+    res.status(err.status || 500)
+    res.setHeader('content-type', 'application/json')
+    res.json({ message: err.message || '', status: err.status || 500 })
+  } else {
+    res.status(400)
+    res.render('index', { title: 'Draft List API' })
+  }
 })
 
 module.exports = app
