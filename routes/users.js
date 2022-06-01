a2009045b68cb0f72fb60e838845727620e6cdac
Andrew Wanex <awanex2705@gmail.com>
2022-06-01 14:41:56 -0700

    refactor: add basic admin route to get users; update signup route to generate a token for response

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,34 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const passport = require('passport')
 const createError = require('http-errors')
 const User = require('../models/User')
 const authenticate = require('../authenticate')
 
 const userRouter = express.Router()
+userRouter.use(bodyParser.json())
 
-userRouter.get('/checkJWT', (req, res) => {
-  passport.authenticate('jwt', {session: false}, (error, user, data) => {
+
+userRouter.route('/')
+  .get(authenticate.verifyUser, (req, res, next) => {
+    User.findById(req.user.id)
+      .then(user => {
+        if (!user) return next(createError(404, 'User not found'))
+
+        const { username, email, deviceList, authoredList } = user
+        res.statusCode = 200
+        res.setHeader('content-type', 'application/json')
+        res.json({
+          username,
+          email,
+          deviceList,
+          authoredList
+        })
+      })
+  })
+
+userRouter.get('/checkUserJWT', (req, res, next) => {
+  passport.authenticate('UserStrategy', {session: false}, (error, user, data) => {
     if (error) return next(error)
 
     if (!user) {
@@ -23,13 +44,13 @@ userRouter.get('/checkJWT', (req, res) => {
       res.statusCode = 200
       res.setHeader('content-type', 'application/json')
       return res.json({
+        user,
         status: 'JWT valid',
         success: true,
-        user: user,
         error: null
       })
     }
-  })(req, res)
+  })(req, res, next)
 })
 
 userRouter.post('/login', (req, res, next) => {
@@ -48,47 +69,47 @@ userRouter.post('/login', (req, res, next) => {
       success: true,
       status: 'login successful',
       user: {
-        id: user.id,
+        _id: user._id,
         username: user.username,
         email: user.email,
-        token: authenticate.getToken({_id: req.user._id})
+        token: authenticate.getUserToken({_id: user._id})
       }
     })
-  })
+  })(req, res, next)
 })
 
-userRouter.post('/signup', async (req, res, next) => {
-  const users = await User.find({})
-  if (users.length > 20) {
-    res.statusCode = 423
-    res.setHeader('content-type', 'application/json')
-    return res.json({success: false, status: 'Max users reached for this version: contact admin'})
-  }
-
-  const parsedDoc = JSON.parse(req.body.user)
-  req.body['username'] = parsedDoc.username
-  req.body['password'] = parsedDoc.password
-
-  User.register(
-    new User({
-      username: parsedDoc.username,
-      email: parsedDoc.email
-    }),
-    parsedDoc.password,
-    (error, user) => {
-      if (error) return next(error)
-
-      req.login(user, error => {
-        if (error) return next(error)
-
-        res.statusCode = 200
+userRouter.post('/signup', (req, res, next) => {
+  User.find({})
+    .then(users => {
+      if (users && users.length > 20) {
+        res.statusCode = 423
         res.setHeader('content-type', 'application/json')
-        res.json({
-          success: true,
-          status: 'registration successful',
-          token: authenticate.getToken({ _id: user._id })
-        })
-      })
-    }
-  )
+        return res.json({success: false, status: 'Max users reached for this version: contact admin'})
+      }
+      return null
+    })
+    .then(() => {
+      User.register(
+        new User({
+          username: req.body.username,
+          email: req.body.email
+        }),
+        req.body.password,
+        (error, user) => {
+          if (error) return next(createError(400, error))
+
+          passport.authenticate('local')(req, res, () => {
+            res.statusCode = 200
+            res.setHeader('content-type', 'application/json')
+            res.json({
+              success: true,
+              status: 'registration successful',
+              token: authenticate.getUserToken({ _id: user._id })
+            })
+          })
+        }
+      )
+    })
 })
+
+module.exports = userRouter
