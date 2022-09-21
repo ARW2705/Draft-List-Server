84d07b386693ff600f668772055feb38d0e06c91
Andrew Wanex <awanex2705@gmail.com>
2022-09-21 15:34:58 -0700

    refactor: remove history from draft

--- models/Draft.js
+++ models/Draft.js
@@ -1,23 +1,17 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const draftContainerSchema = require('./DraftContainer')
-const historySchema = require('./History')
 
 const draftSchema = new Schema({
   author: {
     type: Schema.Types.ObjectId,
     ref: 'User'
   },
-  isActive: {
-    type: Boolean,
-    default: true
-  },
   beverage: {
     type: Schema.Types.ObjectId,
     ref: 'Beverage'
   },
-  container: draftContainerSchema,
-  history: [ historySchema ]
+  container: draftContainerSchema
 }, {
   timestamps: true
 })
