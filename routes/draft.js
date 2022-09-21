00c49706429fa63fb297417141d1b59dedcd1914
Andrew Wanex <awanex2705@gmail.com>
2022-09-21 15:35:52 -0700

    refactor: update draft routes to handle changes to user and device storage with pagination

--- routes/draft.js
+++ routes/draft.js
@@ -11,164 +11,83 @@ draftRouter.use(bodyParser.json())
 
 draftRouter.route('/')
   .get(authenticate.verifyUser, (req, res, next) => {
-    User.findById(req.user.id)
-      .populate({
-        path: 'draftList',
-        populate: {
-          path: 'beverage'
-        }
-      })
-      .populate({
-        path: 'draftList',
-        populate: {
-          path: 'container.containerInfo'
-        }
-      })
-      .then(user => {
-        if (!user) return next(createError(404, 'User not found'))
+    const page = parseInt(req.query.page)
+    const count = parseInt(req.query.count)
 
-        const fullDraftList = []
-        user.deviceList.forEach(device => {
-          device.draftList.forEach(draft => {
-            if (!req.query.isActive || draft.isActive) {
-              fullDraftList.push(draft)
-            }
-          })
-        })
-        return Draft.find({ _id: { $in: fullDraftList } })
-      })
+    Draft.find({})
+      .sort({ _id: 1 })
+      .skip(page * count)
+      .limit(count)
+      .populate('container.containerInfo')
       .then(draftList => {
         res.statusCode = 200
         res.setHeader('content-type', 'application/json')
         res.json(draftList)
-      })
-      .catch(next)
-  })
-
-draftRouter.route('/device/:deviceId')
-  .get(authenticate.verifyUser, (req, res, next) => {
-    Device.findById(req.params.deviceId)
-      .populate('draftList')
-      .then(device => {
-        if (!device) return next(createError(404, 'Device not found'))
-        if (!device.author.equals(req.user.id)) return next(createError(403, 'Device does not belong to user'))
-
-        const fullDraftList = []
-        device.draftList.forEach(draft => {
-          if (!req.query.isActive || draft.isActive) {
-            fullDraftList.push(draft)
-          }
-        })
-
-        return Draft.find({ _id: { $in: fullDraftList } })
-          .populate({
-            path: 'draftList',
-            populate: {
-              path: 'beverage'
-            }
-          })
-          .populate({
-            path: 'draftList',
-            populate: {
-              path: 'container.containerInfo'
-            }
-          })
-      })
-      .then(draftList => {
-        res.statusCode = 200
-        res.setHeader('content-type', 'application/json')
-        res.json(draftList)
-      })
-  })
-  .post(authenticate.verifyUser, (req, res, next) => {
-    Device.findById(req.params.deviceId)
-      .then(device => {
-        if (!device) return next(createError(404, 'Device not found'))
-        if (!device.author.equals(req.user.id)) {
-          return next(createError(403, 'Device does not belong to user'))
-        }
-
-        if (req.body.beverage.contentColor) {
-          req.body.container.contentColor = req.body.beverage.contentColor
-        }
-        Object.assign(req.body, { author: req.user.id })
-        return Draft.create(req.body)
-          .then(draft => {
-            device.draftList.push(draft)
-            return device.save().then(() => draft)
-          })
-      })
-      .then(newDraft => {
-        res.statusCode = 201
-        res.setHeader('content-type', 'application/json')
-        res.json(newDraft)
       })
       .catch(next)
   })
 
 draftRouter.route('/:draftId')
   .get(authenticate.verifyUser, (req, res, next) => {
-    Promise.all([
-      User.findById(req.user.id),
-      Draft.findById(req.params.draftId)
-        .populate({
-          path: 'draftList',
-          populate: {
-            path: 'beverage'
-          }
-        })
-        .populate({
-          path: 'draftList',
-          populate: {
-            path: 'container.containerInfo'
-          }
-        })
-    ])
-    .then(([user, draft]) => {
-      if (!user || !draft) return next(createError(404, `${ user ? 'Draft' : 'User' } not found`))
-
-      if (!user.equals(draft.author)) {
-        return next(createError(400, 'Draft does not belong to the current user'))
-      }
-
-      res.statusCode = 200
-      res.setHeader('content-type', 'application/json')
-      res.json(draft)
-    })
-    .catch(next)
+    Draft.findById(req.params.draftId)
+      .populate('container.containerInfo')
+      .then(draft => {
+        if (!draft) return next(createError(404, 'Draft not found'))
+      
+        res.statusCode = 200
+        res.setHeader('content-type', 'application/json')
+        res.json(draft)
+      })
+      .catch(next)
   })
   .patch(authenticate.verifyUser, (req, res, next) => {
-    Promise.all([
-      User.findById(req.user.id),
-      Draft.findById(req.params.draftId)
-    ])
-    .then(([user, draft]) => {
-      if (!user || !draft) return next(createError(404, `${ user ? 'Draft' : 'User' } not found`))
+    Draft.findByIdAndUpdate(req.params.draftId, { $set: req.body }, { new: true })
+      .populate('container.containerInfo')
+      .then(updatedDraft => {
+        res.statusCode = 200
+        res.setHeader('content-type', 'application/json')
+        res.json(updatedDraft)
+      })
+  })
 
-      if (!user.equals(draft.author)) {
-        return next(createError(400, 'Draft does not belong to the current user'))
-      }
+draftRouter.route('/device/:deviceId')
+  .post(authenticate.verifyUser, (req, res, next) => {
+    Device.findById(req.params.deviceId)
+      .then(device => {
+        if (!device) return next(createError(404, 'Device not found'))
 
-      return Draft.findByIdAndUpdate(req.params.draftId, { $set: req.body }, { new: true })
-        .populate({
-          path: 'draftList',
-          populate: {
-            path: 'beverage'
-          }
-        })
-        .populate({
-          path: 'draftList',
-          populate: {
-            path: 'container.containerInfo'
-          }
-        })
-    })
-    .then(updatedDraft => {
-      res.statusCode = 200
-      res.setHeader('content-type', 'application/json')
-      res.json(updatedDraft)
-    })
-    .catch(next)
+        return Draft.create(req.body)
+          .populate('container.containerInfo')
+          .then(draft => {
+            device.draftList.push(draft)
+            return device.save()
+              .then(() => {
+                res.statusCode = 200
+                res.setHeader('content-type', 'application/json')
+                res.json(draft)
+              })
+          })
+      })
+      .catch(next)
+  })
+
+draftRouter.route('/device/:deviceId/draft/:draftId')
+  .get(authenticate.verifyUser, (req, res, next) => {
+    Device.findById(req.params.deviceId)
+      .then(device => {
+        if (!device.draftList.includes(req.params.draftId)) {
+          return next(createError(400, 'Draft does not belong to device'))
+        }
+
+        return Draft.findById(req.params.draftId)
+          .populate('container.containerInfo')
+      })
+      .then(draft => {
+        res.statusCode = 200
+        res.setHeader('content-type', 'application/json')
+        res.json(draft)
+      })
+      .catch(next)
   })
 
 module.exports = draftRouter
